22ae81ea4c6da61472acdc728ab4bb5e4c9b0163
BorkDaga <nicolas_inostroza68@outlook.es>
2024-04-10 20:44:45 -0400

    Tarea IA modificaciones

--- grafico.py
+++ grafico.py
@@ -3,14 +3,29 @@ import numpy as np
 
 x,y = np.loadtxt('data1.txt', usecols=[0,1], unpack=True)
 
-print(x,y)
+def menu():
+    print("##########################Bienvenido##########################")
+    op = int(input("Ingrese el número de fila para el eje X: "))
+    while(op == 0 or op > 97):
+        op=int(input("Error, reingrese opción: "))
 
-plt.title("Datos IA")
-plt.xlabel("X")
-plt.ylabel("Y")
+    vector1 = (x[op - 1], y[op - 1])
+    print(vector1)
 
-plt.plot(x,y, "*r")
+    op2 = int(input("Ingrese el número de fila para el eje X: "))
+    while(op2 == 0 or op2 > 97):
+        op=int(input("Error, reingrese opción: "))
 
-plt.grid(True)
+    vector2 = (x[op2 - 1], y[op2 - 1])
+    print(vector2)
 
-plt.show()
+    plt.title("Datos IA")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.plot(x,y, "*r")
+    plt.plot(vector1, vector2, linewidth=2, color='b')
+    plt.grid(True)
+
+    plt.show()
+
+menu()

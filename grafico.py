4af659c04ad4f0b130a2fbfc9cb469c81c22483f
BorkDaga <nicolas_inostroza68@outlook.es>
2024-04-17 22:27:43 -0400

    Avance Tarea IA

--- grafico.py
+++ grafico.py
@@ -2,30 +2,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x,y = np.loadtxt('data1.txt', usecols=[0,1], unpack=True)
+m,b = np.polyfit(x, y, deg=1)
 
 def menu():
     print("##########################Bienvenido##########################")
     op = int(input("Ingrese el número de fila para el eje X: "))
-    while(op == 0 or op > 97):
+    while(op == 0 or op > 96):
         op=int(input("Error, reingrese opción: "))
 
-    vector1 = (x[op - 1], y[op - 1])
-    print(vector1)
-
-    op2 = int(input("Ingrese el número de fila para el eje X: "))
-    while(op2 == 0 or op2 > 97):
-        op=int(input("Error, reingrese opción: "))
-
-    vector2 = (x[op2 - 1], y[op2 - 1])
-    print(vector2)
+    ejex = x[op - 1]
+    ejey = y[op - 1]
+    print(ejex, ",", ejey)
 
     plt.title("Datos IA")
     plt.xlabel("X")
     plt.ylabel("Y")
-    plt.plot(x,y, "*r")
-    plt.plot(vector1, vector2, linewidth=2, color='b')
+    plt.axhline(ejey, xmin = 0, xmax = 10, linestyle = 'dotted')
+    plt.axvline(ejex, ymin = 0, ymax = 10, linestyle = 'dotted')
+    plt.legend(bbox_to_anchor = (1.0, 1))
+    plt.plot(x,y, "*")
+    plt.plot(x, m*x + b)
     plt.grid(True)
-
     plt.show()
 
 menu()
